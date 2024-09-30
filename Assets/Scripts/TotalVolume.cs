e29e52ea1512fdb5f1dd7621d49edf30c37c3318
longhoang2403 <140371313+longhoang2403@users.noreply.github.com>
2024-09-30 11:20:40 +0700

    up

--- Assets/Scripts/TotalVolume.cs
+++ Assets/Scripts/TotalVolume.cs
@@ -3,37 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MusicScripts : MonoBehaviour
+public class TotalVolume : MonoBehaviour
 {
-    public Slider _musicSlider;
+    public Slider _allSlider;
     public Sprite newImageNhac; // Ảnh khi tắt âm thanh 
     public Sprite oldImageNhac;     // Ảnh khi mở âm thanh
-                                    // 
     private Image buttonImageNhac; // Biến lưu trữ component Image của nút Nhạc
     // Start is called before the first frame update
-  /*  private void Awake()
-    {
-        _musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 0.5f); // Tải giá trị âm lượng đã lưu hoặc đặt mặc định là 0.5
-        MusicVolume(); // Áp dụng giá trị âm lượng
-        buttonImageNhac = GetComponent<Image>(); // Lấy component Image của nút
-
-        UpdateButtonImage(); // Cập nhật hình ảnh nút
-    }*/
     void Start()
     {
-        bool isMuted = PlayerPrefs.GetInt("MusicMuted", 0) == 1;
-        AudioManager.Instance.musicSource.mute = isMuted;
-        buttonImageNhac = GetComponent<Image>(); // Lấy component Image của nút
+        bool isMuted = PlayerPrefs.GetInt("AllMuted", 0) == 1;
+        AudioManager.Instance.sfxSource.mute = isMuted;
+        buttonImageNhac = GetComponent<Image>();
+        _allSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f); // Tải giá trị âm lượng đã lưu hoặc đặt mặc định là 0.5
+        AllVolume(); // Áp dụng giá trị âm lượng
         UpdateButtonImage();
-        _musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 0.5f);
-        MusicVolume();
     }
+
     public void ToggleMusic()
     {
-       // AudioManager.Instance.PlaySFX("ClickButton");
+        // AudioManager.Instance.PlaySFX("ClickButton");
         bool isMuted = !AudioManager.Instance.musicSource.mute;
+        AudioManager.Instance.sfxSource.mute = isMuted;
+        PlayerPrefs.SetInt("SfxMuted", isMuted ? 1 : 0);
         AudioManager.Instance.musicSource.mute = isMuted;
         PlayerPrefs.SetInt("MusicMuted", isMuted ? 1 : 0);
+        PlayerPrefs.SetInt("AllMuted", isMuted ? 1 : 0);
         AudioManager.Instance.PlaySFX("ClickButton");
         PlayerPrefs.Save();
         UpdateButtonImage();
@@ -43,11 +38,14 @@ public class MusicScripts : MonoBehaviour
         // Cập nhật hình ảnh của button dựa trên trạng thái mute
         buttonImageNhac.sprite = AudioManager.Instance.musicSource.mute ? newImageNhac : oldImageNhac;
     }
-    public void MusicVolume()
+    public void AllVolume()
     {
-        float volume = _musicSlider.value;
+        float volume = _allSlider.value;
         AudioManager.Instance.MusicVolume(volume);
         PlayerPrefs.SetFloat("musicVolume", volume);
+        AudioManager.Instance.SFXVolume(volume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.SetFloat("AllVolume", volume);
         PlayerPrefs.Save();
     }
 }
