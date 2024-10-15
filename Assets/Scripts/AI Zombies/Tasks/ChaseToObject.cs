89e9b7be7a55bc44bf79d7cc13a2e9340a4d12ee
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2024-10-15 21:52:02 +0700

    AI Basic Zombies

---
+++ Assets/Scripts/AI Zombies/Tasks/ChaseToObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class ChaseToObject : Action
+{
+    public float speed = 0;
+    public SharedTransform target;
+    public float chaseRange = 10.0f; // Phạm vi để bắt đầu quay
+
+    public override TaskStatus OnUpdate()
+    {
+        // Kiểm tra khoảng cách giữa hai đối tượng
+        float distance = Vector3.Distance(transform.position, target.Value.position);
+        if (distance <= chaseRange)
+        {
+            // Tính toán hướng quay chỉ theo trục X và Z
+            Vector3 direction = new Vector3(target.Value.position.x - transform.position.x, 0, target.Value.position.z - transform.position.z);
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5.0f);
+        }
+
+        // Trả về trạng thái thành công nếu đã đến mục tiêu
+        if (Vector3.SqrMagnitude(transform.position - target.Value.position) < 0.1f)
+        {
+            return TaskStatus.Success;
+        }
+
+        // Di chuyển về phía mục tiêu
+        transform.position = Vector3.MoveTowards(transform.position, target.Value.position, speed * Time.deltaTime);
+        return TaskStatus.Running;
+    }
+}
