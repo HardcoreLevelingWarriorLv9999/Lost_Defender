971f3c23746aeaa8d2fcef41d36d73b38cc0af69
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2024-10-27 00:06:02 +0700

    Add hitbox and health for zombies

---
+++ Assets/Scripts/AI Zombies/Hitbox.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public ZombieHealth zombieHealth;
+
+    public void OnHit()
+    {
+        
+    }
+}
