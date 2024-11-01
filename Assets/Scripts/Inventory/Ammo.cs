974a01007b12c531a503f6b051c5ce6acf9fb016
TranKhoiNguyen777 <tran.nguyen.vn.257146@gmail.com>
2024-11-01 12:39:42 +0700

    FPS DEMO 1

--- Assets/Scripts/Inventory/Ammo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Ammo : Item
-{
-    private int _amount = 0; public int amount { get { return _amount; } set { _amount = value; } }
-    
-}
