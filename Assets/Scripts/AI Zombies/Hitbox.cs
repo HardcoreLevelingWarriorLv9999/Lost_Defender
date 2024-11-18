598764d63320e31b5797379dbda27d3cf313ca14
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2024-11-18 13:59:23 +0700

    Fix Bug

--- Assets/Scripts/AI Zombies/Hitbox.cs
+++ Assets/Scripts/AI Zombies/Hitbox.cs
@@ -6,8 +6,8 @@ public class Hitbox : MonoBehaviour
 {
     public ZombieHealth zombieHealth;
 
-    public void OnHit(Bullet bullet, Vector3 direction)
-    {
-        zombieHealth.TakeDamage(bullet.bulletDamage, direction);
-    }
+    // public void OnHit(Bullet bullet, Vector3 direction)
+    // {
+    //     zombieHealth.TakeDamage(bullet.bulletDamage, direction);
+    // }
 }
