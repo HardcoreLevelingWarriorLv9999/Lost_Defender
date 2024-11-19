0b4abd3d9b80a309439ad05add2c781df7b5a1d1
longhoang2403 <140371313+longhoang2403@users.noreply.github.com>
2024-11-19 10:51:36 +0700

    up

---
+++ Assets/Scripts/Setting/ControllerSceneCharacter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ControllerSceneCharacter : MonoBehaviour
+{
+    public Image displayImage; // Hình ảnh UI để hiển thị
+    public Sprite[] images; // Mảng chứa 9 tấm hình
+    public string nextSceneName;
+    void Start()
+    {
+        StartCoroutine(ShowImages());
+    }
+
+    IEnumerator ShowImages()
+    {
+        for (int i = 0; i < images.Length; i++)
+        {
+            displayImage.sprite = images[i]; // Hiển thị hình ảnh từ mảng
+            yield return new WaitForSeconds(1); // Đợi 1 giây
+        }
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
