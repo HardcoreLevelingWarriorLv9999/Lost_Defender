12c60dbeef5d22dcebf4c1c9d08543544603374c
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2024-12-13 13:38:53 +0700

    Tạo xong cơ chế spawn zombie cho nhiệm vụ cuối và điều chỉnh map 2 cho nhiệm vụ bảo vệ xe

---
+++ Assets/Scripts/AI Zombies/AutoChaseZombieSpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoChaseZombieSpawner : MonoBehaviour
+{
+    public GameObject[] zombiePrefabs;
+    public RuntimeAnimatorController[] zombieControllers;
+    public Collider[] spawnAreas;  // Use an array of Colliders
+    public List<GameObject> spawnedZombies = new List<GameObject>();
+
+    public void SpawnRandomZombies(int count)
+    {
+        // First, ensure at least one zombie is spawned in each BoxCollider
+        foreach (Collider spawnArea in spawnAreas)
+        {
+            if (count <= 0) break;
+            SpawnRandomZombieInArea(spawnArea);
+            count--;
+        }
+
+        // Spawn remaining zombies randomly
+        for (int i = 0; i < count; i++)
+        {
+            SpawnRandomZombie();
+        }
+    }
+
+    void SpawnRandomZombie()
+    {
+        GameObject zombiePrefab = zombiePrefabs[Random.Range(0, zombiePrefabs.Length)];
+        Vector3 spawnPosition = GetRandomPointInCollider(spawnAreas[Random.Range(0, spawnAreas.Length)]);
+        GameObject newZombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity, transform);
+
+        Animator animator = newZombie.GetComponent<Animator>();
+        RuntimeAnimatorController randomController = zombieControllers[Random.Range(0, zombieControllers.Length)];
+        animator.runtimeAnimatorController = randomController;
+
+        spawnedZombies.Add(newZombie);
+    }
+
+    void SpawnRandomZombieInArea(Collider spawnArea)
+    {
+        GameObject zombiePrefab = zombiePrefabs[Random.Range(0, zombiePrefabs.Length)];
+        Vector3 spawnPosition = GetRandomPointInCollider(spawnArea);
+        GameObject newZombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity, transform);
+
+        Animator animator = newZombie.GetComponent<Animator>();
+        RuntimeAnimatorController randomController = zombieControllers[Random.Range(0, zombieControllers.Length)];
+        animator.runtimeAnimatorController = randomController;
+
+        spawnedZombies.Add(newZombie);
+    }
+
+    Vector3 GetRandomPointInCollider(Collider collider)
+    {
+        Vector3 point;
+        do
+        {
+            point = new Vector3(
+                Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+                Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+        } while (!collider.bounds.Contains(point));
+        return point;
+    }
+}
--- Assets/Scripts/AI Zombies/ZombieSpawner.cs
+++ Assets/Scripts/AI Zombies/ZombieSpawner.cs
@@ -6,20 +6,42 @@ public class ZombieSpawner : MonoBehaviour
 {
     public GameObject[] zombiePrefabs;
     public RuntimeAnimatorController[] zombieControllers;
-    public Collider spawnArea;
-    public int numberOfZombies = 1;
+    public Collider[] spawnAreas;  // Use an array of Colliders
+    public int numberOfZombies = 10;  // Total number of zombies to spawn
 
     public List<GameObject> spawnedZombies = new List<GameObject>();
 
     void Start()
     {
-        for (int i = 0; i < numberOfZombies; i++)
+        // First, ensure at least one zombie is spawned in each BoxCollider
+        foreach (Collider spawnArea in spawnAreas)
+        {
+            SpawnRandomZombieInArea(spawnArea);
+        }
+
+        // Calculate remaining zombies to spawn randomly
+        int remainingZombies = numberOfZombies - spawnAreas.Length;
+
+        for (int i = 0; i < remainingZombies; i++)
         {
             SpawnRandomZombie();
         }
     }
 
     void SpawnRandomZombie()
+    {
+        GameObject zombiePrefab = zombiePrefabs[Random.Range(0, zombiePrefabs.Length)];
+        Vector3 spawnPosition = GetRandomPointInCollider(spawnAreas[Random.Range(0, spawnAreas.Length)]);
+        GameObject newZombie = Instantiate(zombiePrefab, spawnPosition, Quaternion.identity, transform);
+
+        Animator animator = newZombie.GetComponent<Animator>();
+        RuntimeAnimatorController randomController = zombieControllers[Random.Range(0, zombieControllers.Length)];
+        animator.runtimeAnimatorController = randomController;
+
+        spawnedZombies.Add(newZombie);
+    }
+
+    void SpawnRandomZombieInArea(Collider spawnArea)
     {
         GameObject zombiePrefab = zombiePrefabs[Random.Range(0, zombiePrefabs.Length)];
         Vector3 spawnPosition = GetRandomPointInCollider(spawnArea);
