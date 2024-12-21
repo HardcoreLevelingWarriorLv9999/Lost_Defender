b59cf09a04abd05f25824ec4723d9771ad59f5b4
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2024-12-21 17:17:45 +0700

    up

--- Assets/QuestsSystem/Scripts/QuestsSystem/QuestsLogic/FuelUp_QuestLogic.cs
+++ Assets/QuestsSystem/Scripts/QuestsSystem/QuestsLogic/FuelUp_QuestLogic.cs
@@ -10,12 +10,21 @@ namespace QuestsSystem
 
         public override QuestsNames QuestName => QuestsNames.FuelUp;
 
-        public override string QuestTastText => "TASK_TEXT";
+        public override string QuestTastText => $"You have brought {collectedFuel}/{totalFuel} fuel canisters to the car";
+
+        private int collectedFuel = 0;
+        private int totalFuel = 3;
 
         public override void OnAccept()
         {
             QuestsManager.Instance.RemoveQuest(QuestsNames.DefendTheCarToTheGasStation, true);
             OnQuestAccepted?.Invoke(); // Gọi sự kiện khi nhiệm vụ được chấp nhận
+
+            // Object_Interactive[] fuelCanisters = GameObject.FindObjectsOfType<Object_Interactive>(true);
+            // foreach (var canister in fuelCanisters)
+            // {
+            //     canister.gameObject.SetActive(true);
+            // }
         }
 
         public override void Logic()
@@ -25,7 +34,32 @@ namespace QuestsSystem
 
         public override void Progress()
         {
-            //Called when there is logic for progress (Call from any other entity)
+            collectedFuel++;
+            if (collectedFuel == totalFuel)
+            {
+                Complete();
+            }
+        }
+
+        public void DeliverFuelToCar()
+        {
+            // Logic để thả bình xăng vào xe
+            // Ví dụ: kiểm tra người chơi đang đứng gần xe và thả bình xăng vào xe
+            if (IsPlayerNearCar())
+            {
+                collectedFuel++;
+                if (collectedFuel == totalFuel)
+                {
+                    Complete();
+                }
+            }
+        }
+
+        private bool IsPlayerNearCar()
+        {
+            // Kiểm tra vị trí của người chơi và xe
+            // Return true nếu người chơi đang ở gần xe
+            return true; // Thay bằng logic kiểm tra thực tế
         }
 
         public override void OnComplete()
