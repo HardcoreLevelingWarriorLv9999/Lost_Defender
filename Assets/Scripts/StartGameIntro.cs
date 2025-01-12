d21bd99e1d82640088ebfcd18dafc3615c81d100
HardcoreLevelingWarriorLv9999 <153184486+HardcoreLevelingWarriorLv9999@users.noreply.github.com>
2025-01-12 19:59:35 +0700

    Thêm tắt di chuyển và camera khi chạy intro

--- Assets/Scripts/StartGameIntro.cs
+++ Assets/Scripts/StartGameIntro.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using JUTPS;
+using JUTPS.CameraSystems;
 using UnityEngine;
 
 public class StartGameIntro : MonoBehaviour
 {
     public Animator startGameIntroAnimator;
+    public JUCameraController cameraController;
+    public List<JUCharacterController> players;
 
     public void OnAnimationEvent()
     {
         startGameIntroAnimator.SetBool("Active", true);
+        cameraController.enabled = false;
+        players.ForEach(player => player.enabled = false);
     }
 
     public void OnAnimationEvent2()
     {
         startGameIntroAnimator.SetBool("Active", false);
+        cameraController.enabled = true;
+        players.ForEach(player => player.enabled = true);
     }
 }
